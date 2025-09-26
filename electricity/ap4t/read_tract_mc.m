12f058b182512522c5426ab89c430b51248430f5
rrosen4 <rebeccarosen12@gmail.com>
2025-09-26 14:26:27 -0400

    simplified inputs to read_tract_mc(i) by adding global hdf_dir

--- electricity/ap4t/read_tract_mc.m
+++ electricity/ap4t/read_tract_mc.m
@@ -1,5 +1,7 @@
-function [value] = read_tract_mc(i,hdf_dir)
-    % inputs are [i] from (3108,1) Cell? NO groups, they are all datasets - 72357 rows, columns variable
+function [value] = read_tract_mc(i)
+    % set directory as global variable
+    global hdf_dir 
+    % inputs are [i] from (3108,1) Cell (NO groups, they are all datasets (variable # col, 72357)
     file = [hdf_dir 'tract_to_tract.h5'];
     dataset_name = sprintf('/%d', i);  % recreate S._i. naming to be consistent
     
