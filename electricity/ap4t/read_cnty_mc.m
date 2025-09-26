f0c7c5788e1bf98a0f3f1a6554779d207afd29d0
rrosen4 <rebeccarosen12@gmail.com>
2025-09-26 13:41:19 -0400

    renamed read_county_mc.m to read_cnty_mc.m

--- electricity/ap4t/read_cnty_mc.m
+++ electricity/ap4t/read_cnty_mc.m
@@ -1,11 +1,12 @@
-function [value] = read_county_mc(i,j,hdf_dir)
+function [value] = read_cnty_mc(i,j)
+    % set directory as global variable
+    global hdf_dir 
     % inputs are [i,j] from (3,5) Cell
     file = [hdf_dir 'cnty_mc.h5'];
     dataset_name = sprintf('/%d/%d', i, j);  % recreate S._i._j naming to be consistent
     
     % outputs matrix corresponding to i,j
     value = h5read(file, dataset_name); 
-
 end
 
-
+% purpose: read a single value from cnty_mc.h5, e.g. read_cnty_mc(3,5)(1859,3108)
